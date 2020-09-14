2e6cf5a2733cbb634a6965982e3709ca2fb78d9d
시혁 박 <exon.indiv@gmail.com>
2020-09-14 21:57:09 +0900

    init

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const Discord = require("discord.js")
-const { MessageEmbed} = require('discord.js')
+const {MessageEmbed, MessageCollector} = require('discord.js')
 const client = new Discord.Client()
 const setting = require('./setting.json')
 const prefix = "!"
     
 
 
-client.on("ready", () => {
-    console.log("Start")
-    console.log(`--------------------------------------------`);
+client.on('ready', () => {
+    console.log("--Start--")
 });
 
-client.on("message", (msg) => {
-    
+
+client.on('message', (msg) => {
+    if (msg.bot) return;
+    if (msg.content === `${prefix}start`){
+        let arr = create2DArray(10, 10)
+        msg.channel.send(new MessageEmbed()
+            .setTitle('지뢰찾기 시작')
+            .setDescription('TEST')
+        )
+    }
 });
 
 client.login(setting.token)
