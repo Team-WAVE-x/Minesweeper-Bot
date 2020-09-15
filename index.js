c1711d47c29181441558e26a2bf64ed3a9c90868
시혁 박 <exon.indiv@gmail.com>
2020-09-15 21:24:48 +0900

    map

--- index.js
+++ index.js
@@ -11,13 +11,64 @@ client.on('ready', () => {
 });
 
 
+
+const create2DArray = (rows, columns) => {
+    var arr = new Array(rows);
+    for (var i = 0; i < rows; i++) {
+        arr[i] = new Array(columns);
+    }
+    return arr;
+}
+
+let arr = create2DArray(10, 10)
+
+const plusArr = (i, j) => {
+    if ((i >= 0 && i <= 9) && (j >= 0 && j <= 9) && (!(arr[i][j] == 9))) arr[i][j]++
+}
+
+
+
 client.on('message', (msg) => {
     if (msg.bot) return;
     if (msg.content === `${prefix}start`){
-        let arr = create2DArray(10, 10)
+        for (let i = 0; i < 10; i++){
+            for (let j = 0; j < 10; j++){
+                arr[i][j] = 0
+            }
+        }
+
+        for (let i = 0; i < 10; i++){
+            arr[Math.floor(Math.random() * 10)][Math.floor(Math.random() * 10)] = 9
+        }
+        
+        for (let i = 0; i < 10; i++){
+            for (let j = 0; j < 10; j++){
+                if (arr[i][j] == 9) {
+                    plusArr(i+1, j)
+                    plusArr(i+1, j+1)
+                    plusArr(i+1, j-1)
+                    plusArr(i-1, j)
+                    plusArr(i-1, j+1)
+                    plusArr(i-1, j-1)
+                    plusArr(i, j+1)
+                    plusArr(i, j-1)
+                }
+            }
+        }
+
+        let print
+
+        for (let i = 0; i < 10; i++){
+            for (let j = 0; j < 10; j++){
+                print += arr[i][j]
+            }
+            print += "\n"
+        }
+
+        
         msg.channel.send(new MessageEmbed()
-            .setTitle('지뢰찾기 시작')
-            .setDescription('TEST')
+            .setTitle('지뢰찾기')
+            .setDescription(print)
         )
     }
 });
